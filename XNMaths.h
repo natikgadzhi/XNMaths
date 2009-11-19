24810694ab1f5617724c1962b41166ffc2291a44
railsmaniac <iam@railsmaniac.com>
2009-11-20 01:54:25 +0300

    Initial commit.

---
+++ XNMaths.h
@@ -0,0 +1,13 @@
+//
+//  XNMaths.h
+//  XNMaths
+//
+//  Import XNMaths headers.
+//
+//  Created by Нат Гаджибалаев on 20.11.09.
+//  Copyright 2009 Нат Гаджибалаев. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
