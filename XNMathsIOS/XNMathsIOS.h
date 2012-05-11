276014c881e4cf051dca60c73ca9d04db857a824
dodikk <dodikk88.reg@gmail.com>
2012-05-11 11:31:08 +0300

    Added ios target

---
+++ XNMathsIOS/XNMathsIOS.h
@@ -0,0 +1,13 @@
+//
+//  XNMathsIOS.h
+//  XNMathsIOS
+//
+//  Created by MaсServer on 10.05.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XNMathsIOS : NSObject
+
+@end
---
+++ XNMathsIOS/XNMathsIOS.m
@@ -0,0 +1,13 @@
+//
+//  XNMathsIOS.m
+//  XNMathsIOS
+//
+//  Created by MaсServer on 10.05.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "XNMathsIOS.h"
+
+@implementation XNMathsIOS
+
+@end
